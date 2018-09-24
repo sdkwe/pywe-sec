a945c8f03c620431fd5cb085ab3b174a4b0e83d7
Brightcells <brightcells@gmail.com>
2018-09-24 15:36:26 +0800

    :art: Support return bool value

--- pywe_wxa_sec/pywxasec.py
+++ pywe_wxa_sec/pywxasec.py
@@ -11,21 +11,23 @@ class Security(BaseToken):
         # msgSecCheck, Refer: https://developers.weixin.qq.com/miniprogram/dev/api/msgSecCheck.html
         self.WECHAT_MSG_SEC_CHECK = self.API_DOMAIN + '/wxa/msg_sec_check?access_token={access_token}'
 
-    def img_sec_check(self, media_file=None, media_file_path=None, appid=None, secret=None, token=None, storage=None):
-        return self.post(
+    def img_sec_check(self, media_file=None, media_file_path=None, appid=None, secret=None, token=None, storage=None, res_bool_val=False):
+        res = self.post(
             self.WECHAT_IMG_SEC_CHECK.format(access_token=final_access_token(self, appid=appid, secret=secret, token=token, storage=storage)),
             files={
                 'media': media_file or open(media_file_path, 'rb'),
             },
         )
+        return res.get('errcode') == 0 if res_bool_val else res
 
-    def msg_sec_check(self, content, appid=None, secret=None, token=None, storage=None):
-        return self.post(
+    def msg_sec_check(self, content, appid=None, secret=None, token=None, storage=None, res_bool_val=False):
+        res = self.post(
             self.WECHAT_MSG_SEC_CHECK.format(access_token=final_access_token(self, appid=appid, secret=secret, token=token, storage=storage)),
             data={
                 'content': content,
             },
         )
+        return res.get('errcode') == 0 if res_bool_val else res
 
 
 security = Security()
